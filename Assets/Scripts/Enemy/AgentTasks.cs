3bcba05a004e2e8236408ba9c13af9a4f7f86811
Samuel Ting <tingjs05@gmail.com>
2024-05-22 18:53:03 +0800

    updated BT and made more suitable

--- Assets/Scripts/Enemy/AgentTasks.cs
+++ Assets/Scripts/Enemy/AgentTasks.cs
@@ -19,10 +19,164 @@ public class AgentTasks : MonoBehaviour
     }
 
     // priorities tree
+    // death
+    [Task]
+    bool IsDead()
+    {
+        return false;
+    }
+
+    [Task]
+    void Die()
+    {
+
+    }
+
+    // stun
+    [Task]
+    bool IsStunned()
+    {
+        return false;
+    }
+
+    [Task]
+    void Stun()
+    {
+
+    }
+
+    // trap triggered
+    [Task]
+    bool IsTrapTriggered()
+    {
+        return false;
+    }
+
+    [Task]
+    bool IsAtTrapLocation()
+    {
+        return false;
+    }
+
+    [Task]
+    void MoveToTrap()
+    {
+        
+    }
 
     // attack tree
+    // alert
+    [Task]
+    bool IsAlerted()
+    {
+        return false;
+    }
+
+    [Task]
+    void Alert()
+    {
+        
+    }
+
+    // prowl
+    [Task]
+    bool IsPlayerSeen()
+    {
+        return false;
+    }
+
+    [Task]
+    void Prowl()
+    {
+        
+    }
+
+    // hide
+    [Task]
+    bool IsPlayerMovingTowardsSelf()
+    {
+        return false;
+    }
+
+    [Task]
+    void Hide()
+    {
+        
+    }
+
+    // attack
+    [Task]
+    bool IsWithinAttackRange()
+    {
+        return false;
+    }
+
+    [Task]
+    void Attack()
+    {
+        
+    }
+
+    // flee
+    bool HasFledFromPlayer()
+    {
+        return false;
+    }
+
+    [Task]
+    void Flee()
+    {
+        
+    }
 
     // lay trap tree
+    // lay trap
+    bool IsAtCorridor()
+    {
+        return false;
+    }
+
+    [Task]
+    void LayTrap()
+    {
+        
+    }
+
+    // push tree
+    // wait
+    bool IsAtWaitLocation()
+    {
+        return false;
+    }
+
+    [Task]
+    void MoveToWaitLocation()
+    {
+        
+    }
+
+    // push
+    bool IsWithinPushRange()
+    {
+        return false;
+    }
+
+    [Task]
+    void Push()
+    {
+        
+    }
 
     // patrol tree
+    // patrol
+    bool HasReachedTargetLocation()
+    {
+        return false;
+    }
+
+    [Task]
+    void Patrol()
+    {
+        
+    }
 }
